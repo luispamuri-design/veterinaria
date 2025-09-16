bac2eb71b2580687d44370c3f6c669c5e37ed028
luispamuri-diseño <luispamuri@gmail.com>
2025-09-16 16:09:11 -0400

    Subo todos los cambios realizados

--- veterinaria/settings.py
+++ veterinaria/settings.py
@@ -98,7 +98,7 @@ if DEBUG:
 else: 
      DATABASES = {
         'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
+        default=os.environ.get("DATABASE_URL"),
         conn_max_age=600,
         ssl_require=True
     )
